e00b1e79ea55be5611d2fece90235f33a27a6454
Yang <85026759+Yolanda9264@users.noreply.github.com>
2025-04-03 17:14:20 +0800

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ function About() {
         <section id="about" className="about-section">
             <article className="about-content">
                 <h1 className="about-title">About Me</h1>
-                <p>I'm a full-stack developer with over 3 years of experience. My expertise spans both front-end and back-end development. I specialise in React and Next.js for building dynamic, high-performance user interfaces, while also leveraging back-end technologies like Node.js, Java, and PHP to create robust, scalable full-stack solutions.</p>
+                <p>I'm a full-stack developer with extensive experience. My expertise spans both front-end and back-end development. I specialise in React and Next.js for building dynamic, high-performance user interfaces, while also leveraging back-end technologies like Node.js, Java, and PHP to create robust, scalable full-stack solutions.</p>
                 <p>Beyond full-stack development, I’m passionate about digital marketing. I enjoy leveraging SEO, content strategy, and analytics to enhance online visibility and user engagement. My technical background helps me integrate marketing automation and build performance-driven solutions.
                 </p>
             </article>
